becf22bc665c00fc1657eae0893bee2d04c6491f
Val <val.samonte@gmail.com>
2014-11-09 15:29:04 +0800

    module rename

--- app/code/local/ValSamonte/ScheduledProduct/Model/Mysql4/Setup.php
+++ app/code/local/ValSamonte/ScheduledProduct/Model/Mysql4/Setup.php
@@ -2,6 +2,6 @@
 /**
  * Setup model for scheduled product module, extended from catalog module setup
  */
-class EcomDev_ScheduledProduct_Model_Mysql4_Setup extends Mage_Catalog_Model_Resource_Eav_Mysql4_Setup
+class ValSamonte_ScheduledProduct_Model_Mysql4_Setup extends Mage_Catalog_Model_Resource_Eav_Mysql4_Setup
 {
 }
